e545605e767c53f5bccdf928f02157c792abc3f5
asell22 <alexanderjsell@gmail.com>
2016-03-03 20:48:13 +0000

    fixed enctype bug and added post route with multer functionality

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var app = express();
+var multer = require('multer');
+var upload = multer();
 
 app.use(express.static('public'));
+app.use(multer().single('file-to-analyze'));
+
+app.post('/api/fileanalyze', function(req, res, next) {
+   console.log(req.file);
+   res.send(); 
+});
 
 app.listen(process.env.PORT, process.env.IP, function() {
    console.log('Server listening on port', process.env.PORT); 
